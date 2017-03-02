080f352a756fec0e695d300fcbea13c6c35fdf66
Hrishikesh <hrishikesh.deodhar@gmail.com>
2017-03-02 22:39:11 +0530

    Changing the exit code

--- check_container_running.sh
+++ check_container_running.sh
@@ -19,12 +19,10 @@ RUNNING=$(docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null
 if [ $? -eq 1 ]; then
   echo "UNKNOWN - $CONTAINER does not exist."
   docker pull rohitgkk/eureka
-  exit 3
 fi
 
 if [ "$RUNNING" == "false" ]; then
   echo "CRITICAL - $CONTAINER is not running."
-  exit 2
 fi
 
 STARTED=$(docker inspect --format="{{ .State.StartedAt }}" $CONTAINER)
